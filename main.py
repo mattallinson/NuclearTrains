41dc26e451b1e9862ce5ebd25ebb9fb7a6f5becf
Keir Little <me@keirlittle.com>
2016-04-22 12:01:07 +0100

    main scrapes station search page to return list of dicts for each train

--- main.py
+++ main.py
@@ -3,18 +3,46 @@
 import datetime
 
 from bs4 import BeautifulSoup
+import requests
 
 # Configuration
 # !! Never put the API key and secret here !!
 stations = ["CREWSYC"]
-URL_PREFIX = "www.realtimetrains.co.uk/search/advanced"
+URL_PREFIX = "http://www.realtimetrains.co.uk"
 URL_TIME = "0000-2359"
+TIMETABLE_KEYS = ["ind", "plan_arr", "act_arr", "origin", "platform",
+                  "id_url", "toc", "destination", "plan_dep", "act_dep"]
 
 current_date = datetime.date.today()
 url_date = current_date.strftime("%Y/%m/%d")
 
 def get_search_url(station):
-	return "/".join([URL_PREFIX, station, url_date, URL_TIME])
+    return "/".join([URL_PREFIX, "search/advanced", station, url_date, URL_TIME])
+
+def get_train_url(id_url):
+    return URL_PREFIX + id_url
+
+def get_trains(station):
+    trains = []
+    url = get_search_url(station)
+    r = requests.get(url)
+    page = BeautifulSoup(r.text, "html.parser")
+
+    table = page.find("table", attrs={"class":"table table-condensed servicelist advanced"})
+    rows = table.find_all("tr")[1:] # Discard the first table row, as it is the header.
+
+    for row in rows:
+        # The information we want is the text in each table cell.
+        row_data = [td.text for td in row.find_all("td")]
+        # Except ID, which is a link to the train's journey. We want the link address.
+        row_data[5] = row.find("a")["href"]
+
+        trains.append(dict(zip(TIMETABLE_KEYS,row_data)))
+
+    return trains
 
 for station in stations:
-	print(get_search_url(station))
+    print(get_search_url(station))
+    trains = get_trains(station)
+    print(trains)
+    print(get_train_url(trains[0]["id_url"]))
